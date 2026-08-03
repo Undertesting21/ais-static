f1636166bcba22baa0ec347d1f0a41f029536d1c
Undertesting21 <markanthony.palay@Gmail.com>
2026-08-03 13:38:46 +0800

    updated

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/*
+function showOfficial(name, title, image)
+{
+    let modal_overlay = document.querySelector(".modal-overlay");
+    let modal = document.querySelector(".modal");
+
+    img = document.querySelector(".image");
+
+    //let image = "/assets/images/ais_cover.png";
+
+    modal_overlay.classList.remove("hidden");
+    modal.classList.remove("hidden");
+
+    img.src = image;
+}
+*/
